0ce14d50e35fec3c5a79ea98ee03d86b255c3520
unkn0wnNTC <joaovictorcabtalsteam@gmail.com>
2019-09-20 08:22:32 -0300

    updates

--- src/app.js
+++ src/app.js
@@ -1,50 +1,41 @@
 const express = require('express')
-const multer = require('multer')
-const uploadConfig = require('./config/upload');
-const storage = require('@google-cloud/storage');
-const fs = require('fs')
+
+const bodyParser = require('body-parser')
 const app = express()
+const routes = require('./routes')
+app.use(bodyParser.json());
 
-const gcs = storage({
-  projectId: 'even-shuttle-250512',
-  keyFilename: '/keycloud.json'
+
+app.use(routes)
+/*
+const gcsname = req.file.originalname;
+const file = bucket.file(gcsname);
+
+const stream = file.createWriteStream({
+  metadata: {
+    contentType: req.file.mimetype
+  }
 });
-const upload = multer(uploadConfig);
-const bucketName = 'reportai-images'
-const bucket = gcs.bucket(bucketName);
 
-function getPublicUrl(filename) {
-  return 'https://storage.googleapis.com/' + bucketName + '/' + filename;
+stream.on('error', (err) => {
+  req.file.cloudStorageError = err;
+  next(err);
+});
+
+stream.on('finish', () => {
+  req.file.cloudStorageObject = gcsname;
+  req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
+  next();
+});
+
+stream.end(req.file.buffer);
+
+
+fs.unlinkSync(req.file.path);
+
+res.json({ result: getPublicUrl(gcsname) })
 }
+*/
 
-app.post('/insert', upload.single('image'), (req, res) => {
-
-  const gcsname = req.file.originalname;
-  const file = bucket.file(gcsname);
-
-  const stream = file.createWriteStream({
-    metadata: {
-      contentType: req.file.mimetype
-    }
-  });
-
-  stream.on('error', (err) => {
-    req.file.cloudStorageError = err;
-    next(err);
-  });
-
-  stream.on('finish', () => {
-    req.file.cloudStorageObject = gcsname;
-    req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
-    next();
-  });
-
-  stream.end(req.file.buffer);
-
-
-  fs.unlinkSync(req.file.path);
-
-  res.json({ result: getPublicUrl(gcsname) })
-});
 
 app.listen(3333)
